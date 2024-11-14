e4c85053b00875a99bea60801a9f2d9ddf655488
Luc-brou <luc.r.brousseau@gmail.com>
2024-11-14 17:50:10 -0400

    ver 1
    .

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -8,11 +8,29 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    print("Hello! This program will remove certain characters from a word. Input letters separated by commas.")
+
+    new_word=""
+    character_del=[]
+
+    word=input("\nInput a word. ")
+    delete=input("Input the characters you would like to delete. ")
+    character_del.append(delete.lower())
+
+    print(character_del)
 
 
+    for char in character_del:
+        new_word = word.replace(character_del, "_")
+    
+    character_del = delete.split(",")
 
-
-
+    if word.lower()=="stop":
+        print("\nEnding program...")
+        print("Ended.")
+        return
+    
+    print (new_word)
 
 
     # YOUR CODE ENDS HERE
